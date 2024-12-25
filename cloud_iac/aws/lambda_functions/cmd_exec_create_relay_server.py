6c3838c75826687fa795e50e94c289295a8f2e6a
Nico Coetzee <nicc777@gmail.com>
2024-12-25 11:04:02 +0100

    Working on parsing the SQS data

--- cloud_iac/aws/lambda_functions/cmd_exec_create_relay_server.py
+++ cloud_iac/aws/lambda_functions/cmd_exec_create_relay_server.py
@@ -2,6 +2,7 @@ import logging
 import os
 import sys
 import json
+import traceback
 
 
 DEBUG = bool(int(os.getenv('DEBUG', '0')))
@@ -24,7 +25,20 @@ logging.getLogger('boto3').setLevel(logging.CRITICAL)
 logging.getLogger('botocore').setLevel(logging.CRITICAL)
 
 
+def extract_body_as_dict_from_event_record(record)->dict:
+    try:
+        return json.loads(record['body'])
+    except:
+        logger.error('Failed to get body from event: {}'.format(record))
+        logger.debug('EXCEPTION: {}'.format(traceback.format_exc()))
+    return dict()
+
+
 def handler(event, context):
     logger.debug('event: {}'.format(json.dumps(event, default=str)))
+    if 'Records' in event:
+        for record in event['Records']:
+            command_data = extract_body_as_dict_from_event_record(record=record)
+            logger.debug('command_data: {}'.format(json.dumps(command_data, default=str)))
     return "ok"
 
