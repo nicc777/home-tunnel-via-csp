d8b5d322c46efe397f3c443a98986cac77f2b9cc
Nico Coetzee <nicc777@gmail.com>
2024-12-28 22:46:12 +0100

    Determine if this is a new stack or if a change set should be created

--- cloud_iac/aws/lambda_functions/cmd_exec_create_relay_server.py
+++ cloud_iac/aws/lambda_functions/cmd_exec_create_relay_server.py
@@ -3,6 +3,7 @@ import os
 import sys
 import json
 import traceback
+import boto3
 
 
 DEBUG = bool(int(os.getenv('DEBUG', '0')))
@@ -25,6 +26,33 @@ logging.getLogger('boto3').setLevel(logging.CRITICAL)
 logging.getLogger('botocore').setLevel(logging.CRITICAL)
 
 
+def get_deployed_cloudformation_stack_names(next_token: str=None)->list:
+    stack_names = list()
+    client = boto3.client('cloudformation')
+    response = dict()
+    if next_token is not None:
+        response = client.list_stacks(NextToken=next_token)
+    else:
+        response = client.list_stacks()
+    if 'NextToken' in response:
+        stack_names += get_deployed_cloudformation_stack_names(next_token=response['NextToken'])
+    if 'StackSummaries' in response:
+        for stack_summary in response['StackSummaries']:
+            if 'StackName' in stack_summary:
+                if 'DELETE' not in stack_summary['StackStatus']:
+                    if stack_summary['StackName'] not in stack_names:
+                        stack_names.append(stack_summary['StackName'])
+                else:
+                    logger.info(
+                        'Previous version of stack named "{}" found, but ignored as it is in a "{}" state.'.format(
+                            stack_summary['StackName'],
+                            stack_summary['StackStatus']
+                        )
+                    )
+    logger.debug('stack_names: {}'.format(stack_names))
+    return stack_names
+
+
 def extract_body_as_dict_from_event_record(record)->dict:
     try:
         return json.loads(record['body'])
@@ -37,6 +65,7 @@ def extract_body_as_dict_from_event_record(record)->dict:
 def handler(event, context):
     logger.debug('event: {}'.format(json.dumps(event, default=str)))
     if 'Records' in event:
+        current_deployed_stack_names = get_deployed_cloudformation_stack_names()
         for record in event['Records']:
             command_data = extract_body_as_dict_from_event_record(record=record)
             logger.debug('command_data: {}'.format(json.dumps(command_data, default=str)))
@@ -57,6 +86,15 @@ def handler(event, context):
                     ]
                 }
             """
+            if 'StackName' not in command_data or 'StackParameters' not in command_data:
+                raise Exception('Invalid data: {}'.format(command_data))
+            if command_data['StackName'] not in current_deployed_stack_names:
+                # NEW stack
+                logger.info('Creating NEW stack named "{}"'.format(command_data['StackName']))
+            else:
+                # CHANGE SET
+                logger.info('Creating CHANGE SET for stack named "{}"'.format(command_data['StackName']))
+
 
     return "ok"
 
