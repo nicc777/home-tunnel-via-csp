856ac3f4fc0a0b51e660574990e182cd90757343
Nico Coetzee <nicc777@gmail.com>
2024-12-31 12:08:31 +0100

    Bug fixes and documentation updates

--- cloud_iac/aws/lambda_functions/cmd_exec_delete_relay_server_stack.py
+++ cloud_iac/aws/lambda_functions/cmd_exec_delete_relay_server_stack.py
@@ -62,21 +62,23 @@ def extract_body_as_dict_from_event_record(record)->dict:
     return dict()
 
 
-def from_ttl_event_get_stack_name(event: dict)->str:
+def extract_stack_name(record: dict)->str:
     stack_name = None
     try:
-        if event['command'] == 'delete_relay_server_stack':
-            command_parameters = json.loads(event['command_parameters'])
+        if record['command'] == 'delete_relay_server_stack':
+            command_parameters = record['command_parameters']
+            if isinstance(command_parameters, str):
+                command_parameters = json.loads(record['command_parameters'])
             logger.debug('command_parameters: {}'.format(json.dumps(command_parameters, default=str)))
             if 'parameter_name' not in command_parameters or 'parameter_value' not in command_parameters:
-                logger.error('Expected keys in command parameters not found: {}'.format(event['command_parameters']))
+                logger.error('Expected keys in command parameters not found: {}'.format(record['command_parameters']))
             else:
                 if command_parameters['parameter_name'] == 'stack_name':
                     stack_name = command_parameters['parameter_value']
         else:
-            logger.error('Only supported command: "delete_relay_server_stack" - got "{}"'.format(event['command']))
+            logger.error('Only supported command: "delete_relay_server_stack" - got "{}"'.format(record['command']))
     except:
-        logger.error('Failed to get stack name from event: {}'.format(event))
+        logger.error('Failed to get stack name from event: {}'.format(record))
         logger.debug('EXCEPTION: {}'.format(traceback.format_exc()))
     if stack_name is None:
         logger.error('Unable to determine stack name to delete. No action will be taken.')
@@ -101,10 +103,10 @@ def handler(event, context):
         for record in event['Records']:
             command_data = extract_body_as_dict_from_event_record(record=record)
             logger.debug('command_data: {}'.format(json.dumps(command_data, default=str)))
-            # TODO - extract the stack name to delete...
+            stack_name = extract_stack_name(record=command_data)
     elif 'command' in event and 'command_parameters' in event:
         if event['command'] == 'delete_relay_server_stack':
-            stack_name = from_ttl_event_get_stack_name(event=event)
+            stack_name = extract_stack_name(record=event)
     if stack_name is not None:
         if stack_name in current_stacks:
             logger.info('Deleting stack named "{}"'.format(stack_name))
