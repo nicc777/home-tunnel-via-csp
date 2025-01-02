ae28e1f7f15b05ecd05cd1b48efbfa3cdb49f815
Nico Coetzee <nicc777@gmail.com>
2025-01-02 10:48:26 +0100

    Get the instance ID and security group ID from DynamoDB from the supplied event data

--- cloud_iac/aws/lambda_functions/cmd_exec_reconcile_agent_rules.py
+++ cloud_iac/aws/lambda_functions/cmd_exec_reconcile_agent_rules.py
@@ -29,6 +29,8 @@ logger.addHandler(ch)
 logging.getLogger('boto3').setLevel(logging.CRITICAL)
 logging.getLogger('botocore').setLevel(logging.CRITICAL)
 
+logger.propagate = False
+
 
 def scan_dynamodb_table(table_name, filter_expression=None, expression_attribute_values=None):
     # This code obtained via Google Gemini - Slightly modified, but seems to work ok
@@ -52,6 +54,21 @@ def scan_dynamodb_table(table_name, filter_expression=None, expression_attribute
         return None, str(e)
 
 
+def get_records_from_dynamodb(table_name: str, key_begins_with: str):
+    logger.info('Getting records from table "{}" with key starting with "{}"'.format(table_name, key_begins_with))
+    filter_expression = Attr('RecordKey').begins_with(key_begins_with)
+    items, error = scan_dynamodb_table(table_name=table_name, filter_expression=filter_expression)
+    if error is not None:
+        logger.error(
+            'Error while retrieving records with key starting with "{}": {}'.format(
+                key_begins_with,
+                error
+            )
+        )
+        return None
+    return items
+
+
 def extract_records(event)->list:
     records = list()
     try:
@@ -65,9 +82,80 @@ def extract_records(event)->list:
     return records
 
 
+def get_records(relay_id: str, key_begins_with:str)->list:
+    final_key_begins_with = '{}:{}'.format(key_begins_with,relay_id)
+    final_key_begins_with = final_key_begins_with.replace('::', ':')
+    dynamo_db_records = get_records_from_dynamodb(
+        table_name='cumulus-tunnel',
+        key_begins_with='{}'.format(final_key_begins_with)
+    )
+    if dynamo_db_records is not None:
+        logger.debug('dynamo_db_records: {}'.format(json.dumps(dynamo_db_records, default=str)))
+    else:
+        logger.error('None value returned for relay "{}". Skipping.'.format(relay_id))
+        return dict()
+    return dynamo_db_records
+
+
+def get_latest_record(records: list)->dict:
+    latest_record = dict()
+    for record in records:
+        if len(latest_record) == 0:
+            latest_record = copy.deepcopy(record)
+            logger.debug('Setting latest record: {}'.format(json.dumps(record, default=str)))
+        else:
+            if record['RecordTtl'] > latest_record['RecordTtl']:
+                latest_record = copy.deepcopy(record)
+                logger.debug('Found newer record: {}'.format(json.dumps(record, default=str)))
+            else:
+                logger.warning('Ignoring older record: {}'.format(json.dumps(record, default=str)))
+    logger.debug('Final latest_record: {}'.format(json.dumps(latest_record, default=str)))
+    return latest_record
+
+
+def attempt_to_convert_str_to_dict(possible_json_data: str)->dict:
+    try:
+        return json.loads(possible_json_data)
+    except:
+        pass
+    return None
+
+
+def get_instance_id_and_security_group(instance_id_records: list, security_group_records: list)->tuple:
+    instance_id = None
+    security_group_id = None
+
+    try:
+        instance_id_record = get_latest_record(records=instance_id_records)
+        instance_id_record_value = attempt_to_convert_str_to_dict(possible_json_data=instance_id_record['RecordValue'])
+        instance_id = instance_id_record_value['InstanceId']
+
+        security_group_record = get_latest_record(records=security_group_records)
+        security_group_record_value = attempt_to_convert_str_to_dict(possible_json_data=security_group_record['RecordValue'])
+        security_group_id = security_group_record_value['SecurityGroupId']
+
+    except Exception as e:
+        logger.error('Failed to parse data: {}'.format(str(e)))
+        logger.debug('EXCEPTION: {}'.format(traceback.format_exc()))
+
+    return instance_id, security_group_id
+
+
 def handler(event, context):
     logger.debug('event: {}'.format(json.dumps(event, default=str)))
     for record in extract_records(event=event):
         logger.debug('Processing record: {}'.format(json.dumps(record, default=str)))
+        instance_id_records = get_records(relay_id=record['TargetRelayId'], key_begins_with='relay-server:instance-id:')
+        security_group_records = get_records(relay_id=record['TargetRelayId'], key_begins_with='relay-server:security-group:')
+        if len(instance_id_records) == 0 or len(security_group_records) == 0:
+            logger.error('No records for relay "{}" found. Skipping.'.format(record['TargetRelayId']))
+            continue
+        
+        instance_id, security_group_id = get_instance_id_and_security_group(
+            instance_id_records=instance_id_records,
+            security_group_records=security_group_records
+        )
+        
+        logger.info('Latest instance ID is "{}" with security group ID: "{}"'.format(instance_id, security_group_id))
     return "ok"
 
